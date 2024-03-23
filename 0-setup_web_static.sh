868479665453ea0c07072888aea34e3404d63bff
Travis <ondegotravis@gmail.com>
2024-03-23 15:36:44 +0300

    task 0 webb

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# Sets webservers for deployment of webstatic
+apt-get -y update
+apt-get -y upgrade
+apt-get -y install nginx
+mkdir -p /data/web_static/releases/test /data/web_static/shared
+echo "This is a test" | sudo tee /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -hR ubuntu:ubuntu /data/
+sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+service nginx start
